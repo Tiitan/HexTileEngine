219058c212afd563f477f0af55821ad67fab06cb
Florian Noirbent <florian.n@hotmail.fr>
2015-05-12 21:32:50 +0200

    choose painted layer in edit mode and Edge fix 3 (missing files)

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/// <summary>
+/// flag used when editing the map to set wich data should be modified.
+/// </summary>
+[Flags]
+public enum PaintLayer
+{
+	None = 0,
+	Type = 1,
+	Height = 2,
+	All = 3
+}
+
+public static class EnumHelper
+{
+	public static bool Contain<T>(this Enum type, T value)
+	{
+		return (((int)(object)type & (int)(object)value) == (int)(object)value);
+	}
+
+	public static T Include<T> (this Enum type, T value) where T : struct
+	{
+		return (T)(ValueType)(((int)(ValueType) type | (int)(ValueType) value));
+	}
+	
+	public static T Remove<T> (this Enum type, T value) where T : struct
+	{
+		return (T)(ValueType)(((int)(ValueType)type & ~(int)(ValueType)value));
+	}
+}
